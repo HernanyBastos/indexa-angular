820258ee837c0a64a4885467e32d85a03bc6d6f3
Hernany Bastos <hernanybastosfig@gmail.com>
2025-07-01 22:44:00 -0300

    Filtrando pelo input de busca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
 
 
 interface Contato {
@@ -16,7 +17,10 @@ interface Contato {
   telefone: string
 }
 
+
+
 import agenda from './agenda.json';
+
 
 
 @Component({
@@ -24,6 +28,7 @@ import agenda from './agenda.json';
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
@@ -41,8 +46,25 @@ export class AppComponent {
 
   contatos: Contato[] = agenda;
 
+  filtroPorTexto: string = '';
+
+  get letrasComContatosFiltrados(): string[] {
+  return this.alfabeto
+    .split('')
+    .filter(letra => this.filtrarContatoPorLetraIncial(letra).length > 0);
+}
+
+  filtrarContatosPorTexto (): Contato[] {
+    if(!this.filtroPorTexto){
+      return this.contatos;
+    }
+    return this.contatos.filter(contatoUnico => {
+      return contatoUnico.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
+    })
+  }
+
   filtrarContatoPorLetraIncial(letra: string): Contato[]{
-    return this.contatos.filter( contato => {
+    return this.filtrarContatosPorTexto().filter( contato => {
       return contato.nome.toLowerCase().startsWith(letra)
     })
   }
